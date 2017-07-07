340bd8ff5991ff3fb8cd363b00262e8e66807bb6
diky86 <diky86@naver.com>
2017-07-07 12:13:40 +0900

    [*] 패키지명 변경

--- app/src/androidTest/java/com/lpo/seoulnavi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/lpo/seoulnavi/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.ipo.seoulnavi;
+package com.lpo.seoulnavi;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ipo.seoulnavi", appContext.getPackageName());
+        assertEquals("com.lpo.seoulnavi", appContext.getPackageName());
     }
 }
